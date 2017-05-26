5a7f1784f7b9885dc6c26e1e7a0a2c08b8dc44db
guy <luckythelich@hotmail.com>
2017-05-26 09:59:21 -0700

    create return to home link from formContact. Create Post path to ContactSuccess.

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -14,6 +14,10 @@ namespace Contacts.Modules
       Get["FormContact"] = _ => {
         return View["FormContact.cshtml"];
       };
+      Post["FormContact"] = _ => {
+        Contact newContact = new Contact(Request.Form["new-name"], Request.Form["new-number"], Request.Form["new-address"]);
+        return View["ContactSuccess", newContact];
+      }
     }
   }
 }
